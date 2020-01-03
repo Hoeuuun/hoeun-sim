f4c15ffb9a2ca368ac4765468a9567d23ec13a5b
Hoeun Sim <h.sim312@gmail.com>
2020-01-02 18:17:49 -0800

    Add logo

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,12 @@ import styled from 'styled-components';
 export function About() {
     return (
         <div>
-{/*             <img src="../canyon.jpg" alt="Hoeun"> */}
-                <h3>Hi, I'm Hoeun!</h3>
-                <p>Experimental Psychology</p>
-                <p>Researcher</p>
-                <p>turned </p>
-                <p>Programmer.</p>
-                <p> I like to explore and make things.</p>
+            <h3>Hi, I'm Hoeun!</h3>
+            <p>Experimental Psychology</p>
+            <p>Researcher</p>
+            <p>turned </p>
+            <p>Programmer.</p>
+            <p> I like to explore and make things.</p>
          </div>
     );
 }
