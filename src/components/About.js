6ebbdad5a3411161696920aac8c4ea3bcea6574b
Hoeun Sim <h.sim312@gmail.com>
2020-01-11 16:12:57 -0800

    Add intro card

--- src/components/About.js
+++ src/components/About.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Card from 'react-bootstrap/Card';
 
+const GreenText = styled.p`
+    font-size: 2vw;
+    color: #00b386;
+    letter-spacing: 1.5px;
+`;
+
+const BoldText = styled.p`
+    font-size: 3vw;
+    font-weight: bold;
+    letter-spacing: 2px;
+`;
+
+const SmallText = styled.p`
+    font-size: 2.5vw;
+`;
+
+const TitleText = styled.p`
+    font-weight: normal;
+    color: #222;
+    letter-spacing: 4px;
+    font-size: 3.1vw;
+    margin-top: 6vw;
+`;
 
 export function About() {
     return (
         <div>
-            <Card bg="light" style={{ width: '30rem', display: 'flex', flexDirection: 'row'}}>
-                <Card.Img variant="top" src="/canyon.jpg" alt="Hoeun"/>
-                <Card.Body style={{position: 'relative', height: '100%', width: '50%', right: '10px'}}>
-                  <Card.Title><h3>Hi, I'm Hoeun!</h3></Card.Title>
+            <Card bg="white" style={{ margin: '2rem', display: 'flex', flexDirection: 'row'}}>
+                <Card.Img variant="left" src="/canyon.jpg" alt="Hoeun" width="50%"/>
+                <Card.Body style={{position: 'relative'}}>
+                  <Card.Title><TitleText>Hi, I'm Hoeun!</TitleText></Card.Title>
                   <Card.Text>
-                    <p>Experimental Psychology</p>
-                    <p>Researcher</p>
-                    <p>turned </p>
-                    <p>Programmer.</p>
-                    <p> I like to explore and make things.</p>
+                    <SmallText>Experimental Psychology</SmallText>
+                    <BoldText>Researcher</BoldText>
+                    <SmallText>turned </SmallText>
+                    <BoldText>Programmer.</BoldText>
+                    <GreenText> I like to explore and make things.</GreenText>
                   </Card.Text>
                 </Card.Body>
               </Card>
-          <br />
         </div>
     );
 }
