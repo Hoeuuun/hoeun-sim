cc4a2621bd3127cd194f2bd78cdba6cecef463d5
Hoeun Sim <h.sim312@gmail.com>
2020-01-11 17:48:40 -0800

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export function Footer() {
+    return (
+        <footer style={{marginTop: '5em', marginBottom: '1em'}}>
+            <p>&copy; 2020 Hoeun Sim. All rights reserved.</p>
+        </footer>
+    );
+}
